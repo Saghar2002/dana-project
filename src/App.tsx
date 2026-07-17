3416cde5cfad1ea57689ba691920a20302bf6303
Saghar2002 <Sagharrigi81@gmail.com>
2026-07-17 19:23:15 +0330

    start the project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './index.css'
+
+function App() {
+
+
+  return (
+    <>
+   
+      <h1 className="text-black  font-extrabold text-9xl text-white">
+        Hello Saghar 🚀
+      </h1>
+  
+    </>
+  )
+}
+
+export default App
